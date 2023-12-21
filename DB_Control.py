abe5a9544eb6fdfbc45f2a8d098a7b4b1a838866
unknown <ekdud6887@gmail.com>
2023-12-21 09:52:40 +0900

    Studyed

---
+++ DB_Control.py
@@ -0,0 +1,32 @@
+import cxOracle
+import pandas as pd
+
+# DB 연결 함수
+# username : 아이디, password : 암호, dsn : 접속주소
+def db_conn(username, password, dsn) :
+    pass
+
+# DB 연결 해제 함수
+# cur: 커서 객체, conn: 연결 객체
+def db_disconn(conn, cur) :
+    pass
+
+# 데이터 입력 함수
+def insertData() :
+    pass
+
+# 전체 데이터 검색 함수
+def searchAllData() :
+    pass
+
+# 한 개 데이터 검색 함수
+def searchData() :
+    pass
+
+# 데이터 수정 함수
+def updateData() :
+    pass
+
+# 데이터 삭제 함수
+def deleteData() :
+    pass
